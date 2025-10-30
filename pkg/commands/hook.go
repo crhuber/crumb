a8f2bb79e08795143a84817159e02660c35630a7
crhuber <crhuber@users.noreply.github.com>
2025-10-30 16:04:45 +0100

    fix eval in fish

--- pkg/commands/hook.go
+++ pkg/commands/hook.go
@@ -47,7 +47,7 @@ func bashHook(selfPath string) string {
 	return fmt.Sprintf(`_crumb_hook() {
   local previous_exit_status=$?;
   if [ -f .crumb.yaml ]; then
-    eval "$("%s" export --shell bash 2>/dev/null)";
+    eval "$("%s" export --shell bash)";
   fi
   return $previous_exit_status;
 };
@@ -64,7 +64,7 @@ fi
 func zshHook(selfPath string) string {
 	return fmt.Sprintf(`_crumb_hook() {
   if [ -f .crumb.yaml ]; then
-    eval "$("%s" export --shell bash 2>/dev/null)"
+    eval "$("%s" export --shell bash)"
   fi
 }
 typeset -ag precmd_functions
@@ -81,13 +81,13 @@ fi
 func fishHook(selfPath string) string {
 	return fmt.Sprintf(`function _crumb_hook --on-variable PWD --description 'crumb hook'
   if test -f .crumb.yaml
-    eval (%s export --shell fish 2>/dev/null)
+    eval "$(%s export --shell fish)"
   end
 end
 
 function _crumb_hook_prompt --on-event fish_prompt --description 'crumb hook on prompt'
   if test -f .crumb.yaml
-    eval (%s export --shell fish 2>/dev/null)
+    eval (%s export --shell fish)
   end
 end
 `, selfPath, selfPath)
